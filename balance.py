be5942b162bbe6fe19d0c3253747858dcd7d55b5
ZionChang <archerzz.work@gmail.com>
2018-06-25 13:24:45 +0800

    log

--- balance.py
+++ balance.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: zz
 # @Date:   2018-06-24 18:15:55
-# @Last Modified by:   zz
-# @Last Modified time: 2018-06-25 10:44:59
+# @Last Modified by:   zhiz
+# @Last Modified time: 2018-06-25 13:24:31
 
 from fcoin import Fcoin
 from auth import api_key, api_secret
@@ -14,6 +14,7 @@ fcoin = Fcoin(api_key, api_secret)
 # 查询账户余额
 def get_balance_action(symbols):
     balance_info = fcoin.get_balance()
+    print(balance_info)
     for info in balance_info['data']:
     	for symbol in symbols:
 	        if info['currency'] == symbol:
